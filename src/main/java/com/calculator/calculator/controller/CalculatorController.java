e73f80923b4b408b7faed1c5e78462b97383dea5
Aky33 <tom.zaliger@gmail.com>
2025-06-01 12:07:59 +0200

    Refaktoring api

--- src/main/java/com/calculator/calculator/controller/CalculatorController.java
+++ src/main/java/com/calculator/calculator/controller/CalculatorController.java
@@ -1,6 +1,5 @@
 package com.calculator.calculator.controller;
 
-//import com.example.demo.model.User;
 import org.springframework.web.bind.annotation.*;
 
 import com.calculator.calculator.model.Input;
@@ -8,42 +7,18 @@ import com.calculator.calculator.model.IntervalSplaceni;
 import com.calculator.calculator.model.Output;
 import com.calculator.calculator.service.CalculatorService;
 
-//import java.util.*;
-
 @RestController
 @RequestMapping("/")
 public class CalculatorController {
     private CalculatorService service = new CalculatorService();
-
-    //private List<User> users = new ArrayList<>();
 
     @GetMapping("/intervali-splaceni")
     public IntervalSplaceni[] intervaliSplaceni() {
         return IntervalSplaceni.values();
     }
 
-    @PostMapping
+    @PostMapping("/vypocti")
     public Output vypocet(@RequestBody Input input) {
         return service.calculateMortgage(input);
     }
-
-    /*@GetMapping
-    public List<User> getAllUsers() {
-        return users;
-    }
-
-    @PostMapping
-    public User createUser(@RequestBody User user) {
-        user.setId((long) (users.size() + 1));
-        users.add(user);
-        return user;
-    }
-
-    @GetMapping("/{id}")
-    public User getUser(@PathVariable Long id) {
-        return users.stream()
-                .filter(u -> u.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new RuntimeException("User not found"));
-    }*/
 }
